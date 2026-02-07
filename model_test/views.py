4c36b68b0a5f577521174d6cfab99e203d0b919c
momo <doi@jkmomo.com>
2026-02-07 18:07:24 +0900

    クエリ修正

--- model_test/views.py
+++ model_test/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.db.models import OuterRef, Subquery
+from django.db.models import OuterRef, Subquery, Q
 
 from apl.models.models import HaiinfoTbl, KiriTbl
 from .forms import ModelTestForm
@@ -16,6 +16,7 @@ def ModelTestView(request):
     if request.POST:
         form = ModelTestForm(request.POST)
         ctx["form"] = form
+
         if form.is_valid():
             denno = form.cleaned_data["denno"]
 
@@ -26,19 +27,15 @@ def ModelTestView(request):
                 denno=OuterRef("s_denno")
             ).values("den_type")[:1]
 
-            qs = KiriTbl.objects.filter(denno=denno).annotate(
-                den_type=Subquery(den_type_subquery),
-                s_den_type=Subquery(s_den_type_subquery),
-            ).values("denno", "den_type", "s_denno", "s_den_type").order_by("s_denno")
-
-            if not qs.exists():
-                qs = KiriTbl.objects.filter(s_denno=denno).annotate(
-                    den_type=Subquery(den_type_subquery),
-                    s_den_type=Subquery(s_den_type_subquery),
-                ).values("denno", "den_type", "s_denno", "s_den_type").order_by("s_denno")
+            qs = KiriTbl.objects.filter(Q(denno=denno) | Q(s_denno=denno)).values("denno", "s_denno")[:1]
 
             if not qs.exists():
                 return render(request, template_name, ctx)
+            else:
+                qs = KiriTbl.objects.filter(denno=qs[0]["denno"]).annotate(
+                    den_type=Subquery(den_type_subquery),
+                    s_den_type=Subquery(s_den_type_subquery),
+                ).values("denno", "den_type", "s_denno", "s_den_type").order_by("s_denno")
 
             for item in qs:
                 print(item)
